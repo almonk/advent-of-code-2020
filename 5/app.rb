0524580564b7e2de601800cb16a7e49e30f1df5d
Alasdair Monk <alasdair.monk@gmail.com>
2020-12-06 13:08:26 +0000

    Push

---
+++ 5/app.rb
@@ -0,0 +1,39 @@
+raw_boarding_passes = File.readlines "input.txt", "\n"
+@total_rows = 127
+@total_cols = 7
+@boarding_passes = []
+
+def decode_ticket(ticket_arr)
+  previous_row_result = (0..@total_rows).to_a
+  previous_col_result = (0..@total_cols).to_a
+
+  ticket_arr.each do |l|
+    case l
+    when "F"
+      previous_row_result =
+        previous_row_result[0..previous_row_result.length / 2]
+    when "B"
+      previous_row_result =
+        previous_row_result[previous_row_result.length / 2..previous_row_result.length]
+    when "L"
+      previous_col_result =
+        previous_col_result[0..previous_col_result.length / 2]
+    when "R"
+      previous_col_result =
+        previous_col_result[previous_col_result.length / 2..previous_col_result.length]
+    else
+    end
+  end
+
+  return ticket_hash = {
+    row: previous_row_result[0],
+    col: previous_col_result[0],
+    seat_id: (previous_row_result[0] * 8) + previous_col_result[0]
+  }
+end
+
+raw_boarding_passes.each do |ticket|
+  @boarding_passes << decode_ticket(ticket.split(""))
+end
+
+puts @boarding_passes.max_by { |k| k[:seat_id] }
