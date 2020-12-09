a2cc61e73247f74669f3268c81ad567354b4ef02
Alasdair Monk <alasdair.monk@gmail.com>
2020-12-09 11:51:10 +0000

    Optimise

--- 8/app.rb
+++ 8/app.rb
@@ -1,17 +1,18 @@
-require "set"
-
 @input = File.readlines "input.txt", "\n"
 @instructions = []
 
 # Read input into structure
+i = 0
 @input.each do |int|
   int_hash = {
     type: int.split(" ")[0],
     param: int.split(" ")[1],
     has_run: false,
+    index: i
   }
 
   @instructions << int_hash
+  i += 1
 end
 
 def compute_param(param, original_value)
@@ -19,49 +20,45 @@ def compute_param(param, original_value)
   return original_value
 end
 
-# Walk thru instructions
-x = 0
-accumulator_val = 0
+def start_vm(instructions:)
+  # Walk thru instructions
+  x = 0
+  accumulator_val = 0
+  loop do
+    # Check end of input
+    if x >= instructions.length
+      break
+    end
 
-loop do
-  # Check end of input
-  if x >= @instructions.length
-    break
+    # Get instruction
+    instruction = instructions[x]
+
+    # Check instruction has not been run before
+    if instruction[:has_run] == true
+      puts "Detected infinite loop!"
+      break
+    end
+
+    # Process instruction
+    puts "LINE #{x + 1}  \t: #{instruction.to_s}     \t\t#{accumulator_val}"
+
+    case instruction[:type]
+    when "nop"
+    when "acc"
+      accumulator_val = compute_param(instruction[:param], accumulator_val)
+    when "jmp"
+      x = compute_param(instruction[:param], x)
+      next # Skip loop iteration
+    else
+      puts "Did not understand instruction"
+      break
+    end
+
+    # Next instruction
+    instruction[:has_run] = true
+    x += 1
   end
-
-  # Get instruction
-  instruction = @instructions[x]
-
-  # Check instruction has not been run before
-  if instruction[:has_run] == true
-    puts "Detected infinite loop!"
-    break
-  end
-
-  # Process instruction
-  puts "LINE #{x + 1}"
-  puts instruction
-
-  case instruction[:type]
-  when "nop"
-    puts "-- No Operation"
-  when "acc"
-    puts "-- Accumulator"
-    accumulator_val = compute_param(instruction[:param], accumulator_val)
-  when "jmp"
-    puts "-- Jump to"
-    x = compute_param(instruction[:param], x)
-
-    # Skip loop iteration
-    next
-  else
-    puts "Did not understand instruction"
-    break
-  end
-
-  # Next instruction
-  instruction[:has_run] = true
-  x += 1
+  puts accumulator_val
 end
 
-puts accumulator_val
+start_vm(instructions: @instructions)
