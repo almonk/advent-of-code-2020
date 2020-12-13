44a954ac55727ece1dc12a5eb873b3e5004fd516
Alasdair Monk <alasdair.monk@gmail.com>
2020-12-13 19:23:52 +0000

    Fix Day 12 part 1

--- 12/app.rb
+++ 12/app.rb
@@ -1,11 +1,11 @@
-input = File.readlines "input-test", "\n"
+input = File.readlines "input", "\n"
 instructions = []
 
 # Compass directions
 NORTH = "N"
 SOUTH = "S"
 EAST = "E"
-WEST = "w"
+WEST = "W"
 
 # Relative directions
 LEFT = "L"
@@ -34,34 +34,44 @@ loop do
 
   case int[:type]
   when NORTH
+    puts "Move NORTH by #{int[:value]}"
     position_y -= int[:value]
   when SOUTH
+    puts "Move SOUTH by #{int[:value]}"
     position_y += int[:value]
   when EAST
+    puts "Move EAST by #{int[:value]}"
     position_x += int[:value]
   when WEST
+    puts "Move WEST by #{int[:value]}"
     position_x -= int[:value]
   when LEFT
-    rotation -= int[:value]
+    puts "Move LEFT by #{int[:value]}"
+    rotation -= int[:value] % 360
   when RIGHT
-    rotation += int[:value]
+    puts "Move RIGHT by #{int[:value]}"
+    rotation += int[:value] % 360
   when FORWARD
-    if rotation == 0
+    puts "Move FORWARD by #{int[:value]}"
+    if rotation % 360 == 0
       position_y -= int[:value]
-    elsif rotation == 90
+    elsif rotation % 360 == 90
       position_x += int[:value]
-    elsif rotation == 180
+    elsif rotation % 360 == 180
       position_y += int[:value]
-    elsif rotation == 270
+    elsif rotation % 360 == 270
       position_x -= int[:value]
     end
   else
+    puts "Instruction!"
+    break
   end
 
+  puts "Current: x:#{position_x} y:#{position_y} r:#{rotation % 360}"
   i += 1
 end
 
-puts "Facing #{rotation}"
+puts "Facing #{rotation % 360}"
 puts "Position x #{position_x}"
 puts "Position y #{position_y}"
 puts "Answer: #{position_x + position_y}"
